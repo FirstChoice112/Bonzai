584206058441c654da35873d2d9de206ba62fc98
Marcus Widen <hellowiden@gmail.com>
2024-09-16 13:08:19 +0200

    commmmit

--- BonzaiAPI/handler.js
+++ BonzaiAPI/handler.js
@@ -1,5 +1,4 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-
 const {
   DynamoDBDocumentClient,
   GetCommand,
@@ -10,48 +9,45 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-//*Som uuid
-const crypto = require("crypto");
-//let uuid = crypto.randomUUID();
 
 const USERS_TABLE = process.env.USERS_TABLE;
+if (!USERS_TABLE) throw new Error("Environment variable USERS_TABLE must be defined");
+
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
 app.use(express.json());
 
+const handleError = (res, error, statusCode = 500, message = "Internal Server Error") => {
+  console.error(error);
+  res.status(statusCode).json({ error: message });
+};
+
 app.get("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
   const params = {
     TableName: USERS_TABLE,
-    Key: {
-      userId: req.params.userId,
-    },
+    Key: { userId },
   };
 
   try {
     const command = new GetCommand(params);
     const { Item } = await docClient.send(command);
-    if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
-    } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
-    }
+
+    if (!Item) return res.status(404).json({ error: `User with ID "${userId}" not found` });
+
+    const { name } = Item;
+    res.json({ userId, name });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not retrieve user" });
+    handleError(res, error, 500, "Could not retrieve user");
   }
 });
 
 app.post("/users", async (req, res) => {
   const { userId, name } = req.body;
-  if (typeof userId !== "string") {
-    res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
+
+  if (typeof userId !== "string") return res.status(400).json({ error: '"userId" must be a string' });
+  if (typeof name !== "string") return res.status(400).json({ error: '"name" must be a string' });
 
   const params = {
     TableName: USERS_TABLE,
@@ -63,15 +59,10 @@ app.post("/users", async (req, res) => {
     await docClient.send(command);
     res.json({ userId, name });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Could not create user" });
+    handleError(res, error, 500, "Could not create user");
   }
 });
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
+app.use((req, res) => res.status(404).json({ error: "Not Found" }));
 
 exports.handler = serverless(app);
