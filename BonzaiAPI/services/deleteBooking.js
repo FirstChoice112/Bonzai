a979e2593b9a48ae3111396d0f89d648f3aa7345
cozyhorse <100480213+cozyhorse@users.noreply.github.com>
2024-09-17 14:42:42 +0200

    Update deleteBooking.js

--- BonzaiAPI/services/deleteBooking.js
+++ BonzaiAPI/services/deleteBooking.js
@@ -1,7 +1,6 @@
 const { client, docClient, DeleteCommand } = require("../config/config.js");
 
 const deleteBooking = async (bookingid) => {
-<<<<<<< HEAD
     const deleteBooking = new DeleteCommand({
         TableName: process.env.BOOKING_TABLE,
         Key:{
@@ -14,19 +13,3 @@ const deleteBooking = async (bookingid) => {
     return response;
 
 }
-=======
-  const deleteBooking = new DeleteCommand({
-    TableName: process.env.BOOKING_TABLE,
-    Key: {
-      bookingId: bookingid,
-    },
-  });
-
-  const response = await docClient.send(deleteBooking);
-  console.log("RESPONSE", response);
-
-  return response;
-};
-
-module.exports = { deleteBooking };
->>>>>>> origin/dev
