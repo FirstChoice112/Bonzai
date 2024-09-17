5570a4a3a514bbf1693549f99468ee9d0566cb30
cozyhorse <100480213+cozyhorse@users.noreply.github.com>
2024-09-17 11:55:25 +0200

    Update config.js

---
+++ BonzaiAPI/services/config/config.js
@@ -0,0 +1,17 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+  DeleteCommand,
+  ScanCommand,
+  QueryCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
+
+module.exports = {client, docClient};
